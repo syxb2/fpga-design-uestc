17a4e5b444e2a1ee06277242511c9c04b69e327a
syxb2 <578767478@qq.com>
2024-11-23 19:01:50 +0800

    perf: add some module in key_filter
    add counter.v
    add led_encoder_1bit.v
    add top_level.v
    change key_filter_tb.v

---
+++ stage1/key_filter/src/counter.v
@@ -0,0 +1,16 @@
+module counter (clk, rst, in, out);
+    input wire clk;
+    input wire rst;
+    input wire in; // 按键输入信号
+    output reg[3:0] out; // 输出稳定的按键计数信号
+
+    initial begin
+        out = 4'b0000;
+    end
+
+    always @(posedge clk) begin
+        if (in) begin
+            out <= out + 1;
+        end
+    end
+endmodule
--- stage1/key_filter/test/key_filter_tb.v
+++ stage1/key_filter/test/key_filter_tb.v
@@ -4,13 +4,30 @@ module key_filter_tb();
     reg clk;
     reg rst;
     reg in;
-    wire out;
+    wire[6:0] out;
+    wire filter_out;
+    wire[3:0] count;
 
-    key_filter uut(
+    key_filter filter(
         .clk    (clk),
         .rst    (rst),
         .in     (in),
-        .out    (out)
+        .out    (filter_out)
+    );
+
+    counter counter(
+        .clk    (clk),
+        .rst    (rst),
+        .in     (filter_out),
+        .out    (count)
+    );
+
+    led_encoder_1bit led1(
+        .clk    (clk),
+        .rst    (rst),
+        .in     (count),
+        .out    (out),
+        .led    (led)
     );
 
     initial begin
@@ -26,6 +43,20 @@ module key_filter_tb();
         #20 rst = 0;
         in = 0;
         #20 rst = 1;
+        #30 in = 1;
+        #40 in = 0;
+        #50 in = 1;
+        #60 in = 0;
+
+        #3000
+
+        #30 in = 1;
+        #40 in = 0;
+        #50 in = 1;
+        #60 in = 0;
+
+        #3000
+
         #30 in = 1;
         #40 in = 0;
         #50 in = 1;
